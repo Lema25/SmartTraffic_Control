c3c3f7d994fa315b02a4eba43fdb5a5e42db2e50
Lema25 <56377966+Lema25@users.noreply.github.com>
2023-11-22 16:56:01 -0500

    Add files via upload

---
+++ smarttraffic/traffic/models.py
@@ -0,0 +1,27 @@
+
+from django.db import models
+
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+
+class VirtualDevice(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    metadata = models.TextField()
+
+class Thing(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    relationship = models.ManyToManyField('self', blank=True)
+
+class TrafficData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    vehicle_count = models.IntegerField()
+    average_speed = models.FloatField()
+
+    def __str__(self):
+        return f"{self.device.name} - {self.timestamp}"
+    # Otros campos según sea necesario
---
+++ smarttraffic/traffic/views.py
@@ -0,0 +1,86 @@
+# traffic/views.py
+from django.shortcuts import render, redirect
+from .models import Device, VirtualDevice, Thing, TrafficData
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+
+def index(request):
+    devices = Device.objects.all()
+    virtual_devices = VirtualDevice.objects.all()
+    things = Thing.objects.all()
+    traffic_data = TrafficData.objects.all()
+
+    context = {
+        'devices': devices,
+        'virtual_devices': virtual_devices,
+        'things': things,
+        'traffic_data': traffic_data,
+    }
+
+    return render(request, 'traffic/index.html', context)
+
+def device_list(request):
+    devices = Device.objects.all()
+    return render(request, 'traffic/device_list.html', {'devices': devices})
+
+def virtual_device_list(request):
+    virtual_devices = VirtualDevice.objects.all()
+    return render(request, 'traffic/virtual_device_list.html', {'virtual_devices': virtual_devices})
+
+def thing_list(request):
+    things = Thing.objects.all()
+    return render(request, 'traffic/thing_list.html', {'things': things})
+
+def traffic_data_list(request):
+    traffic_data_list = TrafficData.objects.all()
+    print(traffic_data_list)
+    return render(request, 'traffic/traffic_data_list.html', {'traffic_data_list': traffic_data_list})
+
+def my_protected_view(request):
+    # Tu lógica de vista aquí
+    return render(request, 'my_protected_template.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        # Recuperar el nombre de usuario y la contraseña del formulario de inicio de sesión
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Autenticar al usuario
+        user = authenticate(request, username=username, password=password)
+
+        # Verificar si la autenticación fue exitosa
+        if user is not None:
+            # Iniciar sesión para el usuario autenticado
+            login(request, user)
+            # Redirigir a una página después del inicio de sesión (puedes cambiar esto)
+            return redirect('index')
+        else:
+            # Si la autenticación falla, puedes mostrar un mensaje de error
+            error_message = "Nombre de usuario o contraseña incorrectos."
+            return render(request, 'login.html', {'error_message': error_message})
+
+    # Si no es una solicitud POST, simplemente renderizar la página de inicio de sesión
+    return render(request, 'login.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        # Obtener los datos del formulario de registro
+        form = UserCreationForm(request.POST)
+
+        # Verificar si el formulario es válido
+        if form.is_valid():
+            # Crear la cuenta de usuario
+            user = form.save()
+
+            # Iniciar sesión para el nuevo usuario
+            login(request, user)
+
+            # Redirigir a una página después del registro (puedes cambiar esto)
+            return redirect('index')
+    else:
+        # Si no es una solicitud POST, simplemente renderizar el formulario de registro
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
